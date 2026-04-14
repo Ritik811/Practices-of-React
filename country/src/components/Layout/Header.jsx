66784a1cce34f3c373294c6accb0f307bca1a396
Ritik811 <ritikkumar32855@gmail.com>
2026-04-14 13:14:51 +0530

    Start CurrencyConverte Project and finished Basic Setup

--- country/src/components/Layout/Header.jsx
+++ country/src/components/Layout/Header.jsx
@@ -1,3 +1,79 @@
+// import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+// import { GiHamburgerMenu } from "react-icons/gi";
+
+// export const Header = () => {
+//   const [show, setShow] = useState(false);
+
+//   const handleButtonToggle = () => {
+//     return setShow(!show);
+//   };
+
+//   return (
+//     <header>
+//       <div className="container">
+//         <div className="grid navbar-grid">
+//           <div className="Logo">
+//             <NavLink to="/">
+//               <h1>WorldAtlas</h1>
+//             </NavLink>
+//           </div>
+
+//           <nav className={show ? "menu-mobile" : "menu-web"}>
+//             <ul>
+//               <li>
+//                 <NavLink to="/">Home</NavLink>
+//               </li>
+//               <li>
+//                 <NavLink to="/about">About</NavLink>
+//               </li>
+//               <li>
+//                 <NavLink to="/country">Country</NavLink>
+//               </li>
+//               <li>
+//                 <NavLink to="/contact">Contact</NavLink>
+//               </li>
+//             </ul>
+//           </nav>
+//           <div className="ham-menu">
+//             <button onClick={handleButtonToggle}>
+//               <GiHamburgerMenu />
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </header>
+//   );
+// };
+
 export const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <div className="container">
+        <div className="grid nav-grid">
+          <div className="logo">
+            <NavLink to="/">WorldAtLas</NavLink>
+          </div>
+
+          <nav className="grid nav-grid">
+            <ul>
+              <li>
+                <NavLink to="/">Home </NavLink>
+              </li>
+              <li>
+                <NavLink to="/about">About </NavLink>
+              </li>
+              <li>
+                <NavLink to="/country">Country </NavLink>
+              </li>
+              <li>
+                <NavLink to="contact">Contact </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
---
+++ currencyConveter/src/App.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+const App = () => {
+  return <h1>Currency Conveter</h1>;
+};
+
+export default App;
